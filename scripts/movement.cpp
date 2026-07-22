786df3cb472703d765e732f2815c45d0837bf3c7
Ouflix <devouflix@gmail.com>
2026-07-22 21:53:12 +0300

    Update movement.cpp

--- scripts/movement.cpp
+++ scripts/movement.cpp
@@ -8,22 +8,19 @@ const int RPWM_CHANNEL = 0;
 const int LPWM_CHANNEL = 1;
 
 unsigned long motorStartTime = 0;
-const unsigned long RUN_DURATION = 2000; // 1.5 seconds
+const unsigned long RUN_DURATION = 2000;
 bool isRunning = false;
 
 void setup() {
-  // Over USB, we use the main Serial port at 115200 baud
+  
   Serial.begin(115200); 
 
   ledcAttachChannel(RPWM_PIN, PWM_FREQ, PWM_RESOLUTION, RPWM_CHANNEL);
   ledcAttachChannel(LPWM_PIN, PWM_FREQ, PWM_RESOLUTION, LPWM_CHANNEL);
-
-  // Note: While testing with the Pi, keep the Arduino IDE Serial Monitor closed,
-  // because only one device can read a USB serial port at a time!
 }
 
 void loop() {
-  // Read from the USB Serial link
+  
   if (Serial.available() > 0) {
     int targetSpeed = Serial.parseInt(); 
 
